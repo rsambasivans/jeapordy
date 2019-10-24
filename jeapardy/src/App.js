b5645daf8c2a4bb19f4f63240d5b3dd8c38c940f
rsambas1@jhu.edu <jimmyshi360@gmail.com>
2019-10-24 19:35:12 -0400

    added search field

--- jeapardy/src/App.js
+++ jeapardy/src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+}));
 
 function App() {
+   const classes = useStyles();
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +25,25 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+         <form className={classes.container} noValidate autoComplete="off">
+              <TextField
+              id="standard-search"
+              label="Search field"
+              type="search"
+              className={classes.textField}
+              margin="normal"
+              onKeyPress={(ev) => {
+              
+              if (ev.key === 'Enter') {
+                // Do code here
+                console.log("success");
+                ev.preventDefault();
+              }
+              }}
+            />
+
+         </form>
         <a
           className="App-link"
           href="https://reactjs.org"
